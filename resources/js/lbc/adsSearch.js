efc2500576e4f5091f9e55e4a444d4897fc62afe
GrCOTE7 <grcote7@gmail.com>
2024-02-02 06:24:58 +0100

    Nett

--- resources/js/lbc/adsSearch.js
+++ resources/js/lbc/adsSearch.js
@@ -1,19 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config({ path: './.env' });
-
-// import mix from "laravel-mix";
-// import webpack from "webpack";
-// import dotenv from "dotenv-webpack";
-
-// module.exports = {
-//   //...
-//   plugins: [
-//     new webpack.DefinePlugin({
-//       'process.env': JSON.stringify(dotenv.config().parsed)
-//     })
-//   ]
-// };
-
 
 export function adsSearch() {
     let adsSearches = [
