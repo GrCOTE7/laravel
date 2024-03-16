a55529840f2a323e55d1b5432803af4127569fe1
GrCOTE7 <grcote7@gmail.com>
2024-03-16 16:00:50 +0100

    Fix: pages title && footer (no vars)

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title') | L10Vite</title>
+    <title>@yield('title') |  @php echo env('APP_NAME', 'oOo'); @endphp</title>
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -37,8 +37,6 @@
             echo env('APP_NAME', 'oOo');
         @endphp
         (From php block in template)
-        <hr>
-        {{ $appName }} (From var in controller)
 
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
