e77d857382a56e1b245a23f3961bc8c4ea8d05b4
GrCOTE7 <grcote7@gmail.com>
2024-03-20 16:11:22 +0100

    Fix: Path navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,12 +1,12 @@
 <a href="/">Home</a> |
 <a href="/w">W</a> |
-<a href="greg">Greg</a> |
-<a href="export">Export</a> |
-<a href="users">Users</a> |
-<a href="photo">Photo</a> |
-<a href="mail">Mail</a> |
-<a href="files">Files</a> |
-<a href="test">Test</a>
+<a href="/greg">Greg</a> |
+<a href="/export">Export</a> |
+<a href="/users">Users</a> |
+<a href="/photo">Photo</a> |
+<a href="/mail">Mail</a> |
+<a href="/files">Files</a> |
+<a href="/test">Test</a>
  {{-- | <a href="test2">Test2</a> --}}
 <style>
     a{
