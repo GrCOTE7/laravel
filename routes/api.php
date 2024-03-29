02a6491bdcdaf85bd82d540fb0006188daaa80c1
GrCOTE7 <grcote7@gmail.com>
2024-03-29 11:15:01 +0100

    Fix: Api FilmController

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+/**
+ * (ɔ) GrCOTE7 - 1990-2024
+ */
+
+use App\Http\Controllers\Tutos\Sillo\FilmController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 })->middleware('auth:sanctum');
+
+Route::name('api.')->group(function () {
+	Route::apiResource('films', FilmController::class);
+});
